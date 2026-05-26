411546acfc5208d55411f04fd28ead01f6b2afbe
shunsuke <mikasyun1217@gmail.com>
2026-05-27 02:35:26 +0900

    deploy版にnextconfigを修正

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "dvhvadbnjpwsfsyhfczz.supabase.co",
+        hostname: "bkwlnfxzsyttsdwiklzm.supabase.co",
         port: "",
         pathname: "/storage/v1/object/**",
       },
