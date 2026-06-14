ad799b63cc54b670788f175d1bfb432072e8649e
shunsuke <mikasyun1217@gmail.com>
2026-06-14 14:02:10 +0900

    auth-code-errorにリダイレクトされる時のエラーメッセージを表示

--- app/auth/route/route.ts
+++ app/auth/route/route.ts
@@ -23,12 +23,13 @@ export async function GET(request: NextRequest) {
       type,
       token_hash,
     })
+    console.log(error?.message)
     if (!error) {
       // redirect user to specified redirect URL or root of app
       redirect(next)
     }
   }
-
+  
   // redirect the user to an error page with some instructions
   redirect('/auth/auth-code-error')
 }
